e694cba90b5571ebff87bdf0fe39f2397692583f
Pushpendra Babu <code.pushpendra@gmail.com>
2023-11-23 10:27:47 +0530

    changed baseurl

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     
-      <BrowserRouter>
+      <BrowserRouter basename="/Responsive-ToDo">
         <div className='app'>
         <Routes>
             <Route path="/" element={<AddTask />} />
